3b79906755558fe9ff628559049e0baee9f84d29
GlauberAlbz <glauberalmeidab@gmail.com>
2025-10-06 22:06:18 -0300

    Adicionando Filmes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home () {
-    return  <h1>Testando Som!</h1>
+    return <>  
+        <h1>Filmes</h1>
+
+        <p>Interestelar</p>
+        <p>2001: Uma Odisseia no Espaço</p>
+        <p>Gravidade</p>
+        <p>Perdido em Marte</p>
+        <p>Ad Astra - Rumo às Estrelas</p>
+        <p>Contato</p>
+        <p>A Chegada</p>
+        <p>Passageiro Acidental </p>
+        <p>Lunar</p>
+        <p>Apollo 13: Do Desastre ao Triunfo</p>
+        </>
 }
 
 export default Home;
